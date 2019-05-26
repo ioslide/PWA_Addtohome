e6ac3a44dfa5d839f8b1818d6f9ee17ad96a0aea
XHY <41772864+ioslide@users.noreply.github.com>
2019-05-26 15:31:23 +0800

    application

---
+++ js/application.js
@@ -0,0 +1,32 @@
+"use strict";
+
+( function () {
+  addToHomescreen( {
+    autostart: true,
+    autoHide: 0,
+    logging: true,
+    minSessions: 2,
+    onShow: function () {
+      console.log( "showing" );
+    },
+    onInit: function () {
+      console.log( "initializing" );
+    },
+    onAdd: function () {
+      console.log( "adding" );
+    },
+    onInstall: function () {
+      console.log( "Installing" );
+    },
+    onCancel: function () {
+      console.log( "Cancelling" );
+    },
+    displayNextPrime: true,
+    customCriteria: true,
+    displayPace: 0,
+    customPrompt: {
+      src: "icon/android-icon-48x48.png"
+    }
+  } );
+
+} )();
