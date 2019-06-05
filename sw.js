169f3a90a932c5e5755da805247582643aef7c12
XHY <41772864+ioslide@users.noreply.github.com>
2019-06-05 15:59:19 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,32 @@
-const version = "1.94",
+const version = "2.0",
     preCache = "PRECACHE-" + version,
     cacheList = [
-        "/",
+        "/js/addtohomescreen.min.js",
+        "/js/application.js",
+        "/style/addtohomescreen.css"
     ];
 
 /*  Service Worker Event Handlers */
-
 self.addEventListener( "install", function ( event ) {
-
     console.log( "Installing the service worker!" );
-
     self.skipWaiting();
-
     caches.open( preCache )
         .then( cache => {
-
             cache.addAll( cacheList );
-
         } );
 
 } );
 
 self.addEventListener( "activate", function ( event ) {
-
     event.waitUntil(
-
         caches.keys().then( cacheNames => {
             cacheNames.forEach( value => {
-
                 if ( value.indexOf( version ) < 0 ) {
                     caches.delete( value );
                 }
-
             } );
-
             console.log( "service worker activated" );
-
             return;
-
         } )
 
     );
@@ -45,18 +34,13 @@ self.addEventListener( "activate", function ( event ) {
 } );
 
 self.addEventListener( "fetch", function ( event ) {
-
     event.respondWith(
-
         caches.match( event.request )
         .then( function ( response ) {
-
             if ( response ) {
                 return response;
             }
-
             return fetch( event.request );
         } )
     );
-
 } );
